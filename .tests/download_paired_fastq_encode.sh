953ee2d6f6bc7ad05fb9034897c3d0ed663c1328
joreynajr <joreyna@live.com>
2021-04-06 19:38:07 -0700

    Added a test for downloading with ENCODE

---
+++ .tests/download_paired_fastq_encode.sh
@@ -0,0 +1,2 @@
+#snakemake --profile workflow/profiles/local $@ results/main/{cline}/reads/{lib}_1.fastq.gz
+snakemake --profile workflow/profiles/local $@ results/main/T47D/reads/ENCLB183QHG_1.fastq.gz
