f2d621415e5d95f0cb866166cb57af49f4d97474
joreynajr <joreyna@live.com>
2021-04-28 13:44:14 -0700

    Replaced the old combiner script for REfragstats

--- workflow/scripts/combine_multiple_REfragStats.pl
+++ workflow/scripts/combine_multiple_REfragStats.pl
@@ -1,60 +1,45 @@
-# This script will combine all the replicates to generate a signle combined replicate
-
-# Extracting the frag stats
-my @frag_list = @ARGV[0..$#ARGV - 1];
-print("Concating the following REfragStats: @frag_list\n");
-
-# extracting the output file
-my $out_fn = $ARGV[-1];
-chomp ($out_fn);
-
-# merge the split data
-$i = 0;
-while ($i <= $#frag_list){
-        chomp $frag_list[$i];
-        open (in,"$frag_list[$i]");
-        while (<in>){
-                chomp $_;
-                @data = split(/\s+/,$_);
-                if ($frag_list_check{@data[0]}{@data[1]}{@data[2]} eq ""){
-                        $frag_list_check{@data[0]}{@data[1]}{@data[2]} = "yes";
-                        push @frag_list_re,"@data[0]\t@data[1]\t@data[2]\n";
-                }
-		$j = 3;
-		while ($j <= 11){
-			chomp $data[$j];
-			$frag_list_count{$j}{@data[0]}{@data[1]}{@data[2]} += $data[$j];
-			$j++;
-		}
-                undef @data;
+# Super fast perl script to merge REfragStats files
+# Usage: perl combine_multiple_REfragStats.pl <txt file with a list of REfragStat paths> <combined output path>
+my @list = `cat $ARGV[0]`;
+my combined_file = $ARGV[1];
+my %merged;
+my %check;
+my @bed;
+foreach my $l (@list) {
+    chomp $l;
+    print ("$l\n");
+    open(in, "$l");
+    while(my $frag = <in>) {
+        chomp $frag;
+        my $chr = (split(/\s+/,$frag))[0];
+        my $start = (split(/\s+/,$frag))[1];
+        my $end   = (split(/\s+/,$frag))[2];
+        $merged{$chr}{$start}{$end}{1} += (split(/\s+/,$frag))[3];
+        $merged{$chr}{$start}{$end}{2} += (split(/\s+/,$frag))[4];
+        $merged{$chr}{$start}{$end}{3} += (split(/\s+/,$frag))[5];
+        $merged{$chr}{$start}{$end}{4} += (split(/\s+/,$frag))[6];
+        $merged{$chr}{$start}{$end}{5} += (split(/\s+/,$frag))[7];
+        $merged{$chr}{$start}{$end}{6} += (split(/\s+/,$frag))[8];
+        $merged{$chr}{$start}{$end}{7} += (split(/\s+/,$frag))[9];
+        $merged{$chr}{$start}{$end}{8} += (split(/\s+/,$frag))[10];
+        $merged{$chr}{$start}{$end}{9} += (split(/\s+/,$frag))[11];
+        if ($check{$chr}{$start}{$end} eq "" ){
+            push(@bed, "$chr\t$start\t$end");
+            $check{$chr{$start}{$end} = 1;
         }
-        close in;
-        $i++;
+    }
+    close in;
 }
-
-open (out,">$out_fn");
-$i = 0;
-while ($i <= $#frag_list_re){
-        chomp $frag_list_re[$i];
-        @frag_list_re_data = split(/\s+/,$frag_list_re[$i]);
-        $chr   = @frag_list_re_data[0];
-        $start = @frag_list_re_data[1];
-        $end   = @frag_list_re_data[2];
-        if ($chr ne "chrM" && $chr ne "chrY") {
-          print out "$chr\t$start\t$end\t.";
-          $j = 3;
-          while ($j <= 11){
-              $count = int($frag_list_count{$j}{$chr}{$start}{$end}/($#frag_list+1));
-              print out "\t$count";
-              $j
-          }
-          print out "\n";
-        }
-        undef @frag_list_re_data;
-        $i++;
+open(out,">$combined_file");
+foreach my $b (@bed) {
+    chomp $bed;
+    my $chr = (split(/\s+/,$b))[0];
+    my $start = (split(/\s+/,$b))[1];
+    my $end   = (split(/\s+/,$b))[2];
+    print out ("$b");
+    for($i = 1; $i <= 9; $i++){
+        print out ("\t$merged{$chr}{$start}{$end}{$i}");
+    }
+    print out ("\n");
 }
 close out;
-undef %frag_list_check;
-undef %frag_list_count;
-undef @frag_list_re;
-undef @frag_list;
