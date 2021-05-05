c91eba133d2ae53daf5c20d63474d89d3ba83366
joreynajr <joreyna@live.com>
2021-05-04 23:50:11 -0700

    Re-ran hg38_mappability since it somehow was empty

--- .tests/download_hg38_mappability.sh
+++ .tests/download_hg38_mappability.sh
@@ -1,2 +1,2 @@
-snakemake --profile workflow/profiles/local $@ results/refs/hg38_mappability/k50.Umap.MultiTrackMappability.sorted.bedGraph
-#snakemake --profile workflow/profiles/pbs-torque $@ results/refs/hg38_mappability/k50.Umap.MultiTrackMappability.sorted.bedGraph
+#snakemake --profile workflow/profiles/local $@ results/refs/hg38_mappability/k50.Umap.MultiTrackMappability.sorted.bedGraph
+snakemake --profile workflow/profiles/pbs-torque $@ results/refs/hg38_mappability/k50.Umap.MultiTrackMappability.sorted.bedGraph
